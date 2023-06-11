2f6827d7e8a9889e620d98659a61fd0affb77ed7
zonicness <zonicness@hotmail.com>
2023-06-10 18:19:33 -0600

    TodoList completo

---
+++ 01-vite-vanilla/02-react-project/react-project/src/components/HolaMundo.jsx
@@ -0,0 +1,7 @@
+export const HolaMundo = ({nombre, apellido}) => {
+  return (
+    <h2>Yo soy {nombre} {apellido}!</h2>
+  )
+}
+
+export default HolaMundo
---
+++ 01-vite-vanilla/src/components/HolaMundo.js
@@ -0,0 +1,6 @@
+const holaMundo = () => `
+<h1>Hola Mundo</h1>
+<p>Este es un componente de prueba</p>
+`
+
+export default holaMundo
