72901289be9e37d89ec8895f91bbdc8067f08e06
zonicness <zonicness@hotmail.com>
2023-06-25 22:34:53 -0600

    Llamada a API endpoint Trends

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 
 function App () {
   const [gifs, setGifs] = useState([])
+  const apiKey = import.meta.env.VITE_GIPHY_API_KEY // Entorno local de una variable. Aquí estará segura tu APIkey 
 
   // Las llamadas a API normalmente se hacen en el useEffect
   // Usaremos useEffect con un arreglo de dependencias vacío para que se ejecute solo una vez
 
   useEffect(() => {
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=f2hXUfJLRjCD4MHB5ge6hefZID58NM3T&limit=30&offset=0&rating=g&bundle=messaging_non_clips')
-    .then(Response => Response.json())
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=30&offset=0&rating=g&bundle=messaging_non_clips`)
+    .then(response => response.json())
     .then(results => {
       console.log(results.data)
       setGifs(results.data)
