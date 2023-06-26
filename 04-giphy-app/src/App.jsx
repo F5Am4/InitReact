ba799b16cba22bf6e56fda04486120d32c7e847e
zonicness <zonicness@hotmail.com>
2023-06-25 23:52:10 -0600

    SearchBar funcionando

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -1,28 +1,41 @@
 import { useState, useEffect } from 'react'
 import ImageCard from './assets/components/ImageCard'
+import SearchBar from './assets/components/SearchBar'
 import './App.css'
 
 function App () {
   const [gifs, setGifs] = useState([])
-  const apiKey = import.meta.env.VITE_GIPHY_API_KEY // Entorno local de una variable. Aquí estará segura tu APIkey 
+  const apiKey = import.meta.env.VITE_GIPHY_API_KEY // Entorno local de una variable. Aquí estará segura tu APIkey
 
   // Las llamadas a API normalmente se hacen en el useEffect
   // Usaremos useEffect con un arreglo de dependencias vacío para que se ejecute solo una vez
 
+  const sendSearch = (search) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+      .then(response => response.json())
+      .then(results => {
+        console.log(results.data)
+        setGifs(results.data)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }
+
   useEffect(() => {
     fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=30&offset=0&rating=g&bundle=messaging_non_clips`)
-    .then(response => response.json())
-    .then(results => {
-      console.log(results.data)
-      setGifs(results.data)
-    }).catch((error) => {
-      console.error(error)
-    })
+      .then(response => response.json())
+      .then(results => {
+        console.log(results.data)
+        setGifs(results.data)
+      }).catch((error) => {
+        console.error(error)
+      })
   }, [])
 
   return (
     <>
       <div className='App'>
+        <SearchBar handleSearch={sendSearch} />
         <div className='grid-cards'>
           {
             gifs.map(gif => (
@@ -30,13 +43,13 @@ function App () {
                 key={gif.id}
                 url={gif.images.fixed_height.url}
                 title={gif.title}
-               />
-          ))
+              />
+            ))
          }
         </div>
-    </div>
-   </>
+      </div>
+    </>
   )
-
+}
 
 export default App
