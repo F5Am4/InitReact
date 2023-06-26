a9441959a1153d9dd9fb6ec6d5aa749740bb27d6
zonicness <zonicness@hotmail.com>
2023-06-25 21:21:14 -0600

    giphy2

--- 04-giphy-app/src/App.jsx
+++ 04-giphy-app/src/App.jsx
@@ -1,7 +1,22 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
+function App () {
+  const [gifs, setGifs] = useState([])
 
+  // Las llamadas a API normalmente se hacen en el useEffect
+  // Usaremos useEffect con un arreglo de dependencias vacío para que se ejecute solo una vez
+
+  useEffect(() => {
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=f2hXUfJLRjCD4MHB5ge6hefZID58NM3T&limit=30&offset=0&rating=g&bundle=messaging_non_clips')
+    .then(Response => Response.json())
+    .then(results => {
+      console.log(results.data)
+      setGifs(results.data)
+    }).catch((error) => {
+      console.error(error)
+    })
+  }, [])
   return (
     <>
       <h1>Giphy App</h1>
